3115777d9adfd3ab66607afd552d0c83affb2c6b
numbstan04 <2577971132@qq.com>
2021-10-03 15:54:44 +0800

    Initial commit

---
+++ src/main/java/com/klose/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.klose.dao;
+
+import com.klose.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
